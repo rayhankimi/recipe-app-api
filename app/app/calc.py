3656362c89cbbb68abcce70c6c0e07562337b95d
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-08-16 00:29:43 +0800

    Configure Simple Test and TDD practice

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     """Add x and y together"""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract x and y together"""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,7 +6,7 @@ from django.test import SimpleTestCase
 from app import calc
 
 
-class Calcests(SimpleTestCase):
+class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
@@ -14,3 +14,8 @@ class Calcests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting two numbers."""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
