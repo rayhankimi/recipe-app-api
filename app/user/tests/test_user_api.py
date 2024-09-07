ff1cf4dd12f3df8c4052f30d9ab95e14bdf27af9
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-09-08 00:58:58 +0800

    Implement user manager (fix typo)

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -151,5 +151,5 @@ class PrivateUserApiTests(TestCase):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertTrue(self.user.check_password(payload['payload']))
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
