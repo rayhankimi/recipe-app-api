0a83f9329abec65dca500e88c1ca2dc919156071
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-09-10 19:50:02 +0800

    Add recipe API - Fix typo and make migration

--- app/core/models.py
+++ app/core/models.py
@@ -51,11 +51,11 @@ class Recipe(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    title = models.CharField(max_length=255),
-    description = models.TextField(blank=True),
-    time_minutes = models.IntegerField(),
-    price = models.DecimalField(max_digits=5, decimal_places=2),
-    link = models.CharField(max_length=255, blank=True),
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.title
