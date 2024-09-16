f480336238a7e9535686defd3a421334538933d7
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-09-16 17:16:57 +0800

    Implement create recipe using tags using nested Serializers

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,13 +6,38 @@ from rest_framework import serializers
 from core.models import Recipe, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id', ]
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes"""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe, tags is by default read only because
+        nested serializers."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj = Tag.objects.get_or_create(
+                user = auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -20,14 +45,3 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tags."""
-
-    class Meta:
-        model = Tag
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
-
-
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,7 +35,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -44,5 +47,4 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user.order_by('-name'))
-
+        return self.queryset.filter(user=self.request.user).order_by('-name')
