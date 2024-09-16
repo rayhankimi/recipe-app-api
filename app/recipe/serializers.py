88c3bc840da522480b0fc0e04cd662ce59756b72
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-09-16 17:21:05 +0800

    Implement create recipe using tags using nested Serializers

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -31,7 +31,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         recipe = Recipe.objects.create(**validated_data)
         auth_user = self.context['request'].user
         for tag in tags:
-            tag_obj = Tag.objects.get_or_create(
+            tag_obj, created = Tag.objects.get_or_create(
                 user = auth_user,
                 **tag,
             )
